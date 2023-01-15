a557e53c5de3f9d1b7649743ef2f9efd5a1bbb4e
tananine <tana9dev@gmail.com>
2023-01-15 23:47:27 +0700

    add StyledEngineProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import store from '/store/index'
 import '/styles/tailwind.css'
+import { StyledEngineProvider } from '@mui/material'
 import { Provider } from 'react-redux'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <StyledEngineProvider injectFirst>
+        <Component {...pageProps} />
+      </StyledEngineProvider>
     </Provider>
   )
 }
